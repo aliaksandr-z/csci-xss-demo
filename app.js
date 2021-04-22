b2cc2ebef63273a73f181886db8d55392a1f175f
Nicole <nwoje13@gmail.com>
2021-04-22 13:54:24 -0400

    removed custom input validation styling

--- app.js
+++ app.js
@@ -17,8 +17,6 @@ function showPosts() {
 }
 
 const form = document.getElementById("form");
-var validTitle = true;
-var validBody = true;
 
 form.addEventListener("submit", function (event) {
   event.preventDefault();
@@ -28,28 +26,8 @@ form.addEventListener("submit", function (event) {
   // console.log(title, escape(title));
   // console.log(body, escape(body));
 
-  validateInput(title, body);
-
   posts.push({ title: encodeInput(title), body: encodeInput(body) });
   //posts.push({ title: escape(title), body: escape(body) });
   //posts.push({ title: title, body: body });
   showPosts();
 });
-
-function validateInput(str1, str2) {
-  console.log(str1, str2);
-  if (str1.trim() === "") {
-    setError(title, "Title cannot be blank.");
-  }
-
-  if (str2.trim() === "") {
-    setError(body, "Body cannot be blank.");
-  }
-}
-
-function setError(input, message) {
-  const formInput = input.parentElement;
-  const p = formInput.querySelector("p");
-  p.innerText = message;
-  formInput.className = "form-input error";
-}
